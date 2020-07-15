46dd41858e9eca25dac059501d8183ed3a327ec2
harshaabeyvickrama <harshaabeyvickrama@gmail.com>
2020-07-15 19:17:33 +0530

    fghfgh

--- YTv3.js
+++ YTv3.js
@@ -57,7 +57,8 @@ function channelDetails() {
   })
       .then(function(response) {
               // Handle the results here (response.result has the parsed body).
-              console.log("Response", response);
+              //console.log("Response", response);
+              console.log(response.result.items)
             },
             function(err) { console.error("Execute error", err); });
 }
